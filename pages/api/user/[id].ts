b4ac11e3bff21e20ab73b1de74d4c40aebb6e8c6
fran-itria <francoitria01@gmail.com>
2025-09-03 16:08:30 -0300

    Id query

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -2,10 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import getOneUser from "./services/find/getOneUser";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    const { id } = req.query
     const { method } = req
     try {
         if (method == 'GET') {
+            const { id } = req.query
             const user = await getOneUser(id as string)
             res.status(200).json(user)
         }
