2c1ad4f9e779c2100cbf636c58f0ab6483a566c5
fran-itria <francoitria01@gmail.com>
2025-09-03 16:11:03 -0300

    Created more routes for user

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,16 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import createUser from "./services/create/createUser"
 import getAllUsers from "./services/find/getAllUsers"
+import updateUser from "./services/update/updateUser"
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req
+    const { authorization } = req.headers
+    if (!authorization) throw new Error('No se encuentra autorizado')
     try {
-        if (req.method == 'POST') {
+        if (method == 'GET') {
+            const users = await getAllUsers()
+            res.status(200).json(users)
+        }
+        if (method == 'POST') {
             const newUser = await createUser(req.body)
             res.status(200).json(newUser)
         }
-        if (req.method == 'GET') {
-            const users = await getAllUsers()
-            res.status(200).json(users)
+        if (method == 'PUT') {
+            const user = await updateUser(req.body)
+            res.status(200).json({ Message: "Usuario actualizado con exito", user })
         }
     }
     catch (error: any) {
