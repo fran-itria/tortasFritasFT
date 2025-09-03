8fdc98435cffaa5a6b8694d93461964c3bafea6d
fran-itria <francoitria01@gmail.com>
2025-09-03 16:10:18 -0300

    Password hashing and update function creation

--- pages/api/user/services/create/createUser.ts
+++ pages/api/user/services/create/createUser.ts
@@ -1,11 +1,14 @@
 import { User } from "../../../../sequelize/db"
 import { createUserParams } from "./types"
+import bcrypt from "bcrypt";
 
 export default async function createUser(params: createUserParams) {
-    const { name, surname, email, phone, password } = params
+    const { name, surname, email, phone } = params
+    let { password } = params
     if (!name || !surname || !email || !phone || !password) {
         throw new Error('Faltan datos obligatorios')
     }
+    password = bcrypt.hashSync(password, 10);
     const newUser = await User.create({
         name,
         surname,
---
+++ pages/api/user/services/update/updateUser.ts
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+import getOneUser from "../find/getOneUser";
+import { UpdateUserProps } from "./types";
+
+
+export default async function updateUser({
+    id,
+    name,
+    surname,
+    email,
+    phone,
+    password
+}: UpdateUserProps) {
+    if (!id) throw new Error('ID de usuario es obligatorio');
+    if (password) {
+        password = bcrypt.hashSync(password, 10);
+    }
+    const user = await getOneUser(id)
+    await user.update(
+        {
+            name,
+            surname,
+            email,
+            phone,
+            password
+        },
+        {
+            where: {
+                id
+            }
+        },
+    )
+    if (user) return user
+    else throw new Error('No se pudo actualizar el usuario')
+}
