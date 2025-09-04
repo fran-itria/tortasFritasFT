84323620068b514e65911d3f2ccc79241b5761bd
fran-itria <francoitria01@gmail.com>
2025-09-04 14:03:10 -0300

    Include the Order model in get users and get a user

--- pages/api/user/services/find/getAllUsers.ts
+++ pages/api/user/services/find/getAllUsers.ts
@@ -1,7 +1,7 @@
-import { User } from "../../../../sequelize/db";
+import { Order, User } from "../../../../sequelize/db";
 
 export default async function getAllUsers() {
-    const users = await User.findAll()
+    const users = await User.findAll({ include: [Order] })
     if (users.length > 0) return users
     else throw new Error('No hay usuarios registrados')
 }
--- pages/api/user/services/find/getOneUser.ts
+++ pages/api/user/services/find/getOneUser.ts
@@ -1,7 +1,7 @@
-import { User } from "../../../../sequelize/db";
+import { Order, Product, User } from "../../../../sequelize/db";
 
 export default async function getOneUser(id: string) {
-    const user = await User.findByPk(id)
+    const user = await User.findByPk(id, { include: [Order] })
     if (user) return user
     else throw new Error('Usuario no encontrado')
 }
