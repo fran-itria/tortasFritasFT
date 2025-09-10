43a566c139ab778564a15e04cc75a75c1abf2ba3
fran-itria <francoitria01@gmail.com>
2025-09-10 14:46:46 -0300

    route and function to create income

---
+++ pages/api/admin/incomes/create.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withAdminAuth from "../validateAdminUser";
+import createIncome from "./services/createIncome";
+
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const { method } = req
+        if (method !== 'POST') throw new Error('Método HTTP no permitido')
+        const income = await createIncome(req.body)
+        res.status(201).json(income)
+    } catch (error: any) {
+        if (error.message.includes('invalid input value for enum')) {
+            res.status(400).json({ message: "Categoria del ingreso inválida" })
+        } else if (error.message) res.status(405).json({ message: error.message })
+        else res.status(500).json({ error: 'Internal server error' })
+    }
+}
+
+export default withAdminAuth(handler, 'No tienes permisos para crear ingresos');
