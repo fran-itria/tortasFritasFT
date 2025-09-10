10c17d31d37b61e6f8ac1103e0e86c684dba4a09
fran-itria <francoitria01@gmail.com>
2025-09-10 14:45:49 -0300

    routes to get incomes

---
+++ pages/api/admin/incomes/index.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withAdminAuth from "../validateAdminUser";
+import getAllIncomes from "./services/getAllIncomes";
+
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const { method } = req
+        if (method !== 'GET') throw new Error('Método HTTP no permitido')
+        const incomes = await getAllIncomes()
+        res.status(200).json(incomes)
+    } catch (error: any) {
+        if (error.message) res.status(400).json({ error: error.message })
+        else res.status(500).json({ error: 'Internal server error' })
+    }
+}
+
+export default withAdminAuth(handler, 'No tienes permisos para ver los ingresos');
