8e0789a7f4f87aaf529599520a39763b505906fc
fran-itria <francoitria01@gmail.com>
2025-10-07 20:43:18 -0300

    Delete methods

--- packages/backend/src/options/options.controller.ts
+++ packages/backend/src/options/options.controller.ts
@@ -16,27 +16,4 @@ export class OptionsController {
             throw new NotFoundException('No se encontraron opciones');
         }
     }
-
-    @Post()
-    async createOptions(@Req() req: express.Request, @Res() res: express.Response) {
-        const newOptions = await this.optionsService.createOptions()
-        if (newOptions) {
-            res.status(201).json(newOptions);
-        } else {
-            throw new NotFoundException('No se ha podido crear las opciones');
-        }
-    }
-
-    @Delete()
-    async deleteOptions(@Req() req: express.Request, @Res() res: express.Response) {
-        const { id } = req.body
-        await this.optionsService.deleteOptions(id)
-        res.status(200).json({ message: "Configuración eliminada correctamente" })
-    }
-
-    @Put()
-    async updateOptions(@Req() req: express.Request, @Res() res: express.Response) {
-        await this.optionsService.updateOptions(req.body)
-        res.status(200).json({ message: "Configuración actualizada correctamente" })
-    }
 }
