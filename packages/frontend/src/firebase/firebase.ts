45c520579d1473d4b794a7ad164bd47a5888b495
fran-itria <francoitria01@gmail.com>
2025-10-09 12:14:19 -0300

    Variables de entorno

--- packages/frontend/src/firebase/firebase.ts
+++ packages/frontend/src/firebase/firebase.ts
@@ -6,12 +6,12 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAsbbRkTq4HCil9QtX5KKbibL2B1UZKHww",
-  authDomain: "tortasfritasft.firebaseapp.com",
-  projectId: "tortasfritasft",
-  storageBucket: "tortasfritasft.firebasestorage.app",
-  messagingSenderId: "396615403131",
-  appId: "1:396615403131:web:188ed89f66ac3f1e9084de",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN_FIREBASE,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID_FIREBASE,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID_FIREBASE,
+  appId: process.env.NEXT_PUBLIC_APP_ID_FIREBASE,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
