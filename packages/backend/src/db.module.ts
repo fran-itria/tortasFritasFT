85d58cd8929657fefabbe79483f5613fcab2c7e8
fran-itria <francoitria01@gmail.com>
2025-11-21 15:28:58 -0300

    Connect to database

--- packages/backend/src/db.module.ts
+++ packages/backend/src/db.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 
+process.loadEnvFile()
 @Module({
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: '8284',
-      database: 'tortas_fritas_ft',
+      uri: process.env.PG_DATABASE_URL,
       logging: false,
       native: false,
       autoLoadModels: true,
