f3dd16856a145a5599e7a5a3c8ad3962a55d1434
fran-itria <francoitria01@gmail.com>
2025-11-26 02:16:22 -0300

    const errors and waring alerts

--- packages/frontend/src/utils/constAlerts.ts
+++ packages/frontend/src/utils/constAlerts.ts
@@ -4,7 +4,6 @@ export enum constAlerts {
     ORDER_STATUS_UPDATED = 'El estado del pedido se ha actualizado correctamente',
     PRODUCT_ADDED_TO_CART = 'Producto agregado al carrito',
 
-    // error alerts
+    // waring alerts
     MUST_BE_LOGGED_IN = 'Debes estar logeado para agregar productos al carrito',
-    USER_INACTIVE = 'Se encuentra inactivo de la aplicacion'
 }
---
+++ packages/frontend/src/utils/constErrors.ts
@@ -0,0 +1,4 @@
+
+export enum Errors {
+    USER_INACTIVE = 'Se encuentra inactivo de la aplicacion'
+}
